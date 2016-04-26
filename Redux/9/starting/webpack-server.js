bcefd4461d5a476b445beff779d5ab9b1fc7039d
Tamizhvendan S <tamizh88@gmail.com>
2016-04-26 20:08:36 +0530

    Getting Started with Redux CRUD

---
+++ Redux/9/starting/webpack-server.js
@@ -0,0 +1,23 @@
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.config');
+
+config.entry = [
+  config.entry,
+  'webpack-dev-server/client?http://0.0.0.0:3000',
+  'webpack/hot/only-dev-server'
+];
+
+config.plugins =
+  config.plugins.concat(new webpack.HotModuleReplacementPlugin());
+
+config.module.loaders[0].loaders.unshift("react-hot");
+
+var server = new WebpackDevServer(webpack(config), {
+  publicPath : config.output.publicPath,
+  proxy: {
+    "*" : "http://localhost:5000"
+  }
+})
+
+server.listen(3000);
