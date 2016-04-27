adba5c4c3c13fc52a41b8264b44d36bde5c3eaeb
Tamizhvendan S <tamizh88@gmail.com>
2016-04-27 10:18:31 +0530

    Adding Home Component

--- Redux/9/finished/app/contacts/home.jsx
+++ Redux/9/finished/app/contacts/home.jsx
@@ -1,8 +1,75 @@
-var React = require('react');
+import React from 'react';
+import {Table, Grid, Row, Col, Panel, Glyphicon, Button} from 'react-bootstrap';
 
 class Home extends React.Component {
+
+  fakeContacts() {
+    return [
+      {
+        "id": 1,
+        "firstName": "John",
+        "lastName": "Smith",
+        "email": "jsmith@jsmith.com"
+      },
+      {
+        "id": 2,
+        "firstName": "Paul",
+        "lastName": "Adams",
+        "email": "adams@jsmith.com"
+      }
+    ];
+  }
+
   render () {
-    return <div>List of contacts!</div>
+    let contacts = this.fakeContacts()
+    return (
+      <Grid>
+        <Row>
+          <Col xsOffset={9} xs={3}>
+            <Panel>
+              <Button bsStyle="primary" bsSize="large">
+                Create New Contact               
+              </Button>
+            </Panel>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12}>
+            <ContactsTable contacts={contacts} />
+          </Col>
+        </Row>
+      </Grid>
+    );
+  }
+}
+
+class ContactRow extends React.Component {
+  render () {
+    let contact = this.props.contact
+    let name = `${contact.firstName}, ${contact.lastName}`
+    return (
+      <tr>
+        <td>{name}</td>
+        <td>View | Edit | Delete </td>
+      </tr>
+    );
+  }
+}
+
+class ContactsTable extends React.Component {
+  render() {
+    let rows = this.props.contacts.map(c => {
+      return <ContactRow key={c.id} contact={c} />
+    });
+
+    return(
+      <Table striped bordered condensed hover>
+        <thead>
+          <tr><th>Name</th><th>Actions</th></tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </Table>
+    );
   }
 }
 
